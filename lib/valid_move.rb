d10974356686433ef5eb4edaf1bb16b9a3a34fd6
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-01 05:55:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  a = index > 9 || index < 1
+  a = index > 8 || index < 0
   if position_taken?(board, index) && a
     return false
   else
